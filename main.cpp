b2628c32a37a9df585247c72c6b9f82b6849e7b0
Kiryl Pryhozhy <kirill19962003@gmail.com>
2023-11-23 13:38:10 +0300

    complete task using std::thread

--- main.cpp
+++ main.cpp
@@ -5,6 +5,8 @@
 #include <fstream>
 #include <thread>
 #include <mutex>
+#include <unistd.h>
+#include <atomic>
 
 using namespace std;
 
@@ -34,7 +36,25 @@ struct MyCompare {
 
 vector<char> code;
 map<char, vector<char>> dictionary;
+mutex mtx;
+bool isLoading = false;
 map<char, int> symbolsTable;
+
+void animation() {
+    const int sleepFor = 500'000;
+    while (true) {
+        while (isLoading) {
+            usleep(sleepFor);
+            std::cout << "\b\\" << std::flush;
+            usleep(sleepFor);
+            std::cout << "\b|" << std::flush;
+            usleep(sleepFor);
+            std::cout << "\b/" << std::flush;
+            usleep(sleepFor);
+            std::cout << "\b-" << std::flush;
+        }
+    }
+}
 
 void buildDictionary(Node *root) {
     if (root->left != nullptr) {
@@ -56,10 +76,13 @@ void buildDictionary(Node *root) {
 
 void buildDictionaryLog(Node *root) {
     auto start = chrono::high_resolution_clock::now();
+    cout << "Building dictionary: ";
+    isLoading = true;
     buildDictionary(root);
     auto stop = chrono::high_resolution_clock::now();
     auto duration = chrono::duration_cast<chrono::microseconds>(stop - start);
-    cout << "Build dictionary: " << duration.count() << " microseconds" << endl;
+    isLoading = false;
+    cout << "\b" << duration.count() << " microseconds" << endl;
 }
 
 void printDecoded(Node *root, string encodedDataFilePath) {
@@ -113,12 +136,16 @@ void writeEncodedToFile(ifstream &inputFile, string outputFilePath) {
     fileToOutput.close();
 }
 
+
 void writeEncodedToFileLog(ifstream &inputFile, string outputFilePath) {
     auto start = chrono::high_resolution_clock::now();
+    cout << "Writing encoded to file: ";
+    isLoading = true;
     writeEncodedToFile(inputFile, outputFilePath);
     auto stop = chrono::high_resolution_clock::now();
-    auto duration = chrono::duration_cast<chrono::microseconds>(stop - start);
-    cout << "Write to encoded file: " << duration.count() << " microseconds" << endl;
+    auto duration = chrono::duration_cast<chrono::microseconds>(stop - start);\
+    isLoading = false;
+    cout << "\b" << duration.count() << " microseconds" << endl;
 }
 
 Node *buildTree(list<Node *> treeNodes) {
@@ -138,11 +165,28 @@ Node *buildTree(list<Node *> treeNodes) {
 
 Node *buildTreeLog(list<Node *> treeNodes) {
     auto buildTreeStart = chrono::high_resolution_clock::now();
+    cout << "Building huffman tree: ";
+    isLoading = true;
     Node *root = buildTree(treeNodes);
     auto buildTreeStop = chrono::high_resolution_clock::now();
     auto buildTreeDuration = chrono::duration_cast<chrono::microseconds>(buildTreeStop - buildTreeStart);
-    cout << "Build tree: " << buildTreeDuration.count() << " microseconds" << endl;
+    isLoading = false;
+    cout << "\b" << buildTreeDuration.count() << " microseconds" << endl;
     return root;
+}
+
+void countSymbols(const string &chunk) {
+    map<char, int> localTable;
+
+    for (char current: chunk) {
+        localTable[current]++;
+    }
+
+    for (const auto &entry: localTable) {
+        mtx.lock();
+        symbolsTable[entry.first] += entry.second;
+        mtx.unlock();
+    }
 }
 
 list<Node *> createTreeNodes() {
@@ -156,29 +200,66 @@ list<Node *> createTreeNodes() {
     return treeNodes;
 }
 
-void readFromFileSequential(ifstream &inputFile) {
-    while (!inputFile.eof()) {
-        char current = inputFile.get();
-        symbolsTable[current]++;
+void readFromFile(ifstream &inputFile, const int numThreads) {
+    inputFile.seekg(0, ios::end);
+    streampos fileSize = inputFile.tellg();
+    inputFile.seekg(0, ios::beg);
+
+    vector<thread> threads;
+
+    // Read the file in chunks and process each chunk in a separate thread
+    const streampos chunkSize = fileSize / numThreads;
+    for (int i = 0; i < numThreads; ++i) {
+        string chunk(chunkSize, '\0');
+        inputFile.read(&chunk[0], chunkSize);
+        threads.emplace_back(countSymbols, chunk);
     }
+
+    // Wait for all threads to finish
+    for (auto &thread: threads) {
+        thread.join();
+    }
+
+    // Process the remaining data (if any) in the main thread
+    string remainingChunk(fileSize % numThreads, '\0');
+    inputFile.read(&remainingChunk[0], remainingChunk.size());
+    countSymbols(remainingChunk);
 }
 
-void readFromFileSequentialLog(ifstream &inputFile) {
+void readFromFileLog(ifstream &inputFile, const int numThreads) {
     auto start = chrono::high_resolution_clock::now();
-    readFromFileSequential(inputFile);
+    cout << "Read from file: ";
+    isLoading = true;
+    readFromFile(inputFile, numThreads);
     auto stop = chrono::high_resolution_clock::now();
     auto duration = chrono::duration_cast<chrono::microseconds>(stop - start);
-    cout << "Read from file sequential: " << duration.count() << " microseconds" << endl;
+    isLoading = false;
+    cout << "\b" << duration.count() << " microseconds" << endl;
 }
 
 int main(int argc, char *argv[]) {
-    if (argc != 3) {
-        cout << "Arguments format: <threads number> <path to input file> <path to output file>";
+    if (argc != 5) {
+        cout << argc << "\n";
+        for (int i = 0; i < argc; i++)
+            cout << argv[i] << " ";
+        cout << "\n";
+        cout << "Arguments format: <threads number> <path to input file> <path to output file> "
+                "<y if print result to stdout, no otherwise>\n";
         return 1;
     }
-    const int numThreads = std::stoi(argv[0]);
-    const string inputPath = argv[1];
-    const string outputPath = argv[2];
+    const int numThreads = std::stoi(argv[1]);
+    const string inputPath = argv[2];
+    const string outputPath = argv[3];
+    const string needPrint = argv[4];
+    bool print;
+    if (needPrint == "y") {
+        print = true;
+    } else {
+        print = false;
+    }
+
+    thread anim(animation);
+    anim.detach();
 
     ifstream inputFile(inputPath, ios::out | ios::binary);
     if (!inputFile.is_open()) {
@@ -186,13 +267,14 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
-    readFromFileSequentialLog(inputFile);
-//    readFromFileParallelLog(inputFile, numThreads);
+    readFromFileLog(inputFile, numThreads);
     list<Node *> treeNodes = createTreeNodes();
     Node *root = buildTreeLog(treeNodes);
     buildDictionaryLog(root);
     writeEncodedToFileLog(inputFile, outputPath);
     inputFile.close();
-//    printDecoded(root, "/media/kirya/linux/projects/AVS/output.txt");
+    if (print) {
+        printDecoded(root, "/media/kirya/linux/projects/AVS/output.txt");
+    }
     return 0;
 }
